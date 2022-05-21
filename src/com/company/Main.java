84a88b7b4f6fbe8fa9a43ca32819bdaaebf8d535
fslnhv <bgerald30@gmail.com>
2022-05-21 21:51:42 +0300

    Lab 1 assignment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,18 @@
+
 package com.company;
 
 public class Main {
 
     public static void main(String[] args) {
         double gravity = -9.81; // Earth's gravity in m/s^2
-        double initialVelocity = 0.0;
-        double fallingTime = 10.0;
-        double initialPosition = 0.0;
-        double finalPosition = 0.0;
+        double initialVelocity = 10.0;
+        double fallingTime = 5;
+        double initialPosition = 150;
+        double finalPosition;
+
+        finalPosition = 0.5 * (gravity * fallingTime * fallingTime) + (initialVelocity * fallingTime) + initialPosition;
+        finalPosition = (double)(Math.round(finalPosition * 10.0))/10.0;
+
         System.out.println("The object's position after " + fallingTime +
                 " seconds is " + finalPosition + " m.");
     }
