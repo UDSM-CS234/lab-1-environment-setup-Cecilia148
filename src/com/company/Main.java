7dce9e9ffd7afbd84d34adaa335615dcf619e6e8
Cecilia148 <105952913+Cecilia148@users.noreply.github.com>
2022-05-28 23:52:28 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,9 @@ public class Main {
 
     public static void main(String[] args) {
         double gravity = -9.81; // Earth's gravity in m/s^2
-        double initialVelocity = 10.0;
-        double fallingTime = 5;
-        double initialPosition = 150;
+        double initialVelocity = 5.0;
+        double fallingTime = 3;
+        double initialPosition = 170;
         double finalPosition;
 
         finalPosition = 0.5 * (gravity * fallingTime * fallingTime) + (initialVelocity * fallingTime) + initialPosition;
